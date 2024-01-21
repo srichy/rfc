7f24021fcba8b4c2ab3f38f3927859934aa79213
Steven Rich <srichy@mac.com>
2024-01-21 15:40:37 -0500

    skip_ws functional

--- src/main.rs
+++ src/main.rs
@@ -24,23 +24,25 @@ struct Fth {
 
 struct InputMgr {
     input_readers: Vec<BufReader<File>>,
+    last_char: Option<u8>,
 }
 
 impl InputMgr {
-    fn new() -> Self {
+    pub fn new() -> Self {
         InputMgr {
             input_readers: Vec::new(),
+            last_char: None,
         }
     }
 
-    fn open_file(&mut self, filename: &str) -> anyhow::Result<()> {
+    pub fn open_file(&mut self, filename: &str) -> anyhow::Result<()> {
         let f = File::open(filename)?;
         let r = BufReader::new(f);
         self.input_readers.push(r);
         Ok(())
     }
 
-    fn close_current(&mut self) -> anyhow::Result<bool> {
+    pub fn close_current(&mut self) -> anyhow::Result<bool> {
         match self.input_readers.pop() {
             None => Ok(false),
             Some(_) => {
@@ -49,33 +51,71 @@ impl InputMgr {
         }
     }
 
-    fn str_by(&mut self, break_when: impl Fn(u8) -> bool) -> anyhow::Result<String> {
-        let mut buf = Vec::new();
-
-        'read_loop: loop {
-            let mut char_buf = [0; 1];
-
-            match self.input_readers.last_mut() {
-                None => return Ok(String::new()),
-                Some(reader) => {
-                    let read_size = reader.read(&mut char_buf)?;
-                    if read_size == 0 {
-                        self.close_current()?;
-                        continue 'read_loop;
+    fn next_char(&mut self) -> anyhow::Result<Option<u8>> {
+        match self.last_char.take() {
+            Some(c) => Ok(Some(c)),
+            None => {
+                'read_loop: loop {
+                    match self.input_readers.last_mut() {
+                        None => return Ok(None),
+                        Some(reader) => {
+                            let mut read_buf = [0; 1];
+                            let n_read = reader.read(&mut read_buf)?;
+                            if n_read == 0 {
+                                self.close_current()?;
+                                continue 'read_loop;
+                            } else {
+                                return Ok(Some(read_buf[0]))
+                            }
+                        }
                     }
-                    if break_when(char_buf[0]) {
-                        // In this application (Forth), there is no need to
-                        // "unread" the delimiter char.
-                        let r_str = String::from_utf8(buf)?;
-                        return Ok(r_str);
-                    }
-                    buf.push(char_buf[0]);
                 }
             }
         }
     }
 
-    fn word(&mut self) -> anyhow::Result<String> {
+    pub fn skip_ws(&mut self) -> anyhow::Result<()> {
+        loop {
+            match self.next_char()? {
+                None => return Ok(()),
+                Some(c) => {
+                    if (c as char).is_whitespace() {
+                        continue
+                    }
+                    self.last_char = Some(c);
+                    return Ok(());
+                }
+            };
+        }
+    }
+
+    pub fn str_by(&mut self, break_when: impl Fn(u8) -> bool) -> anyhow::Result<Option<String>> {
+        let mut buf = Vec::new();
+
+        loop {
+            match self.next_char()? {
+                None => {
+                    if buf.len() > 0 {
+                        let r_str = String::from_utf8(buf)?;
+                        return Ok(Some(r_str));
+                    } else {
+                        return Ok(None);
+                    }
+                }
+                Some(c) => {
+                    if break_when(c) {
+                        // In this application (Forth), there is no need to
+                        // "unread" the delimiter char.
+                        let r_str = String::from_utf8(buf)?;
+                        return Ok(Some(r_str));
+                    }
+                    buf.push(c);
+                }
+            }
+        }
+    }
+
+    pub fn word(&mut self) -> anyhow::Result<Option<String>> {
         self.str_by(|c: u8| (c as char).is_whitespace())
     }
 }
@@ -86,11 +126,12 @@ fn main() -> anyhow::Result<()> {
     im.open_file(&cli.filename)?;
 
     loop {
+        im.skip_ws()?;
         let w = im.word()?;
-        if w.len() == 0 {
-            break;
+        match w {
+            None => break,
+            Some(w) => println!("@@@ {w}"),
         }
-        println!("{w}");
     }
 
     Ok(())
